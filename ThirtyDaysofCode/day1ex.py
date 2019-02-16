8fec82ec6fc20d09b37c555ef969bb002f3c1e0b
n-hari159 <n.hari159@gmail.com>
2019-02-16 22:05:37 +0530

    New file added

---
+++ ThirtyDaysofCode/day1ex.py
@@ -0,0 +1,19 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+
+# Read and save an integer, double, and String to your variables.
+
+# Print the sum of both integer variables on a new line.
+
+# Print the sum of the double variables on a new line.
+
+# Concatenate and print the String variables on a new line
+# The 's' variable above should be printed first.
+i1=int(input()) 
+d2=float(input())
+s1=input()
+print(i+i1)
+print(d+d2)
+print(s+s1)
